a39e139af8de77273540cabef573ee7d9c021972
Rachel S <queenuvchaos@gmail.com>
2019-10-18 05:51:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// write your code below!
+function happyHolidays(){
+  return "Happy holidays!"
+}// write your code below!
+function happyHolidaysTo($name){
+    console.log('Happy holidays, $(name)!');
+}
+function happyHolidayTo(holiday, name){
+  return "Happy " + holiday + "!";
+}
+function holidayCountdown(holiday, days){
+  return "It's ${days} days until $[holiday}"
+}
